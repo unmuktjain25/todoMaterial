48e1e15028f7f9b821a233ae217aa602795cb4af
unmuktjain25 <unmuktjain.rera@gmail.com>
2022-12-25 21:09:47 +0530

    listTable component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListTableComponent } from './list-table/list-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/list-table/list-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-table',
+  templateUrl: './list-table.component.html',
+  styleUrls: ['./list-table.component.css']
+})
+export class ListTableComponent {
+
+}
