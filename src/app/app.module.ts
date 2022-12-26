495f9bd82a89241dbea794bbad440a9c437b431c
unmuktjain25 <unmuktjain.rera@gmail.com>
2022-12-26 19:28:34 +0530

    commit discription

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListTableComponent } from './list-table/list-table.component';
+import { FirstComponent } from './lib/menu/first/first.component';
+import { FooterComponent } from './lib/menu2/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListTableComponent
+    ListTableComponent,
+    FirstComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { ListTableComponent } from './app/list-table/list-table.component';
 import { AppModule } from './app/app.module';
 
 
